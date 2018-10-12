8710b706a3c34cd80f976cabab4f5c135e37a635
qx <1302993490@qq.com>
2018-10-12 18:59:34 +0800

    qx web.php修改

--- routes/web.php
+++ routes/web.php
@@ -14,21 +14,19 @@
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('home/common',function(){
+Route::get('/home/common',function(){
 	return view('/common/home');
 });
 
 
-//后台公共页面
+//后台公共页面d
 Route::get('/admin/common','admin\IndexController@common');
-
 //前台公共页面
 Route::get('/common/home','home\IndexController@cmn');
 
 //前台登录
 Route::any('/home/login','home\LoginController@login');
 Route::any('/home/dologin','home\LoginController@dologin');
-
 
 //前台路由组
 Route::group([],function()
@@ -37,25 +35,16 @@ Route::group([],function()
 	Route::get('/home/friends','home\FriendsController@friends');
 	Route::get('/home/face','home\FriendsController@face');
 	Route::get('/home/pwd','home\FriendsController@pwd');
-
-
-
 	//系统维护
 	Route::get('admin/mai','admin\IndexController@mai');
-
 	//前台选项卡
 	Route::get('/home/index','home\IndexController@index');
 	Route::get('/home/user/update','home\IndexController@update');
 	Route::get('/common/home','home\IndexController@profile');
 	Route::post('/home/user/face','home\IndexController@face');
 	Route::get('/home/user/pwd','home\IndexController@pwd');
-
-
 	//前台修改个人信息
 	Route::get('/home/user/profile','home\UserController@profile');
-
-
-
 });
 
 
@@ -67,7 +56,6 @@ Route::group([],function ()
 {
 	//后台首页
 	Route::get('/admin/index','admin\IndexController@Index');
-
 	//后台系统设置
 	Route::get('/admin/sys/web','admin\SysController@web');
 	Route::get('/admin/sys/aud','admin\SysController@aud');
@@ -81,8 +69,6 @@ Route::group([],function ()
 	Route::resource('admin/friends','admin\FriendsController');
 	//轮播图
 	Route::resource('/admin/slideshows','admin\SlideShowsController');
-
 	//后台用户管理
 	Route::resource('admin/user','admin\UserController');
-
 });
